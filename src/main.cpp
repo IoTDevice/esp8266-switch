d9ce2682eb1a9782bea257aa3a10bfa71d1dc9f1
Farry <yu@iotserv.com>
2019-08-04 11:12:45 +0800

    api

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,59 @@
 #include <ESP8266mDNS.h>
 
 ESP8266WebServer server(80);
-// 看你的继电器是连接那个io，默认gpio2
-const int led = 2;
+// 看你的继电器是连接那个io，默认gpio0
+const int led1 = 0;
+const int led2 = 0;
+// 如果是gpio0
 // const int led = 0;
 // 开关的状态表示
 const int on = 1;
 const int off = 0;
+// 开关的当前状态
+int led1status = off;
+int led2status = off;
+// digitalWrite(led1, on);
 
+// web服务器的根目录
 void handleRoot() {
-  digitalWrite(led, on);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, off);
+  server.send(200, "text/plain", "this is index page from esp8266!");
 }
 
+void handleLEDStatusChange(){
+  String message = "{\"code\":0,\"message\":\"success\"}";
+  for (uint8_t i=0; i<server.args(); i++){
+    if (server.argName(i)=="pin")
+    {
+      if (server.arg(i)=="ON1")
+      {
+        digitalWrite(led1, on);
+        led1status = on;
+      }else if (server.arg(i)=="OFF1")
+      {
+        digitalWrite(led1, off);
+        led1status = off;
+      }else if (server.arg(i)=="ON2")
+      {
+        digitalWrite(led2, on);
+        led2status = on;
+      }else if (server.arg(i)=="OFF2")
+      {
+        digitalWrite(led2, off);
+        led2status = off;
+      }
+    }
+  }
+  server.send(200, "text/plain", message);
+}
+
+void handleCurrentLEDStatus(){
+  String message;
+  message = "{\"led1\":"+String(led1status)+",\"led2\":"+String(led2status)+"}";
+  server.send(200, "application/json", message);
+}
+
+// 页面或者api没有找到
 void handleNotFound(){
-  digitalWrite(led, on);
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -31,12 +69,14 @@ void handleNotFound(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
-  digitalWrite(led, off);
 }
 
 void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  pinMode(led1, OUTPUT);
+  digitalWrite(led1, off);
+  pinMode(led2, OUTPUT);
+  digitalWrite(led2, off);
+
   // Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   // WiFi.begin(ssid, password);
@@ -59,11 +99,8 @@ void setup(void){
   }
 
   server.on("/", handleRoot);
-
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
-  });
-
+  server.on("/led", handleLEDStatusChange);
+  server.on("/status", handleCurrentLEDStatus);
   server.onNotFound(handleNotFound);
 
   server.begin();
