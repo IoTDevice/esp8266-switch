75347f41a0d27a70bd5c5496f5782cc57104ebc5
Farry <newfarry@126.com>
2019-09-28 23:14:51 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,32 @@ String deviceName = "esp8266 Switch";
 String version = "1.0";
 ESP8266WebServer server(httpPort);
 // 看你的继电器是连接那个io，默认gpio0
-const int led1 = 0;
-// 如果是gpio0
-// const int led = 0;
+const int ledPin = 0;
 // 开关的状态表示
 const int on = 1;
 const int off = 0;
 // 开关的当前状态
-int led1status = off;
-// digitalWrite(led1, on);
+String ledStatus = "off";
+// digitalWrite(led, on);
 
 // web服务器的根目录
 void handleRoot() {
   server.send(200, "text/html", "<h1>this is index page from esp8266!</h1>");
 }
 // 操作LED开关状态的API
-void handleLEDStatusChange(){
+void handleLedStatusChange(){
   String message = "{\"code\":0,\"message\":\"success\"}";
   for (uint8_t i=0; i<server.args(); i++){
-    if (server.argName(i)=="pin")
+    if (server.argName(i)=="status")
     {
-      if (server.arg(i)=="ON1")
+      if (server.arg(i)=="on")
       {
-        digitalWrite(led1, on);
-        led1status = on;
-      }else if (server.arg(i)=="OFF1")
+        digitalWrite(ledPin, on);
+        ledStatus = "on";
+      }else if (server.arg(i)=="off")
       {
-        digitalWrite(led1, off);
-        led1status = off;
+        digitalWrite(ledPin, off);
+        ledStatus = "off";
       }
     }
   }
@@ -52,9 +50,9 @@ void handleDeviceRename(){
   server.send(200, "application/json", message);
 }
 // 当前的LED开关状态API
-void handleCurrentLEDStatus(){
+void handleCurrentledStatus(){
   String message;
-  message = "{\"led1\":"+String(led1status)+",\"code\":0,\"message\":\"success\"}";
+  message = "{\"led\":\""+ledStatus+"\",\"code\":0,\"message\":\"success\"}";
   server.send(200, "application/json", message);
 }
 // 设备信息
@@ -100,28 +98,23 @@ void setup(void){
 //   WiFi.begin(ssid, password);
    WiFi.beginSmartConfig();
 
-  pinMode(led1, OUTPUT);
-  // digitalWrite(led1, off);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, off);
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     // Serial.print(".");
   }
-  // Serial.println("");
-  // Serial.print("Connected to ");
-  // Serial.println(ssid);
-  // Serial.print("IP address: ");
-  // Serial.println(WiFi.localIP());
 
   if (MDNS.begin("esp-switch-"+String(ESP.getFlashChipId()))) {
     // Serial.println("MDNS responder started");
   }
 
   server.on("/", handleRoot);
-  server.on("/led", handleLEDStatusChange);
+  server.on("/led", handleLedStatusChange);
   server.on("/rename", handleDeviceRename);
-  server.on("/status", handleCurrentLEDStatus);
+  server.on("/status", handleCurrentledStatus);
   // about this device
   server.on("/info", handleDeviceInfo);
   server.onNotFound(handleNotFound);
